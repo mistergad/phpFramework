3ffbaaf7a8dd39e0f0ec4639f9c39f8be76d4acf
Gadji <audi-tore05@mail.ru>
2017-06-02 23:03:14 +0300

    Inception

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus-pc
+ * Date: 16.05.2017
+ * Time: 16:19
+ */
+
+    $query = $_SERVER['QUERY_STRING'];
+
+    require '../vendor/core/Router.php';
+
+    $router = new Router();
+
+?>
---
+++ vendor/core/Router.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: asus-pc
+ * Date: 16.05.2017
+ * Time: 16:31
+ */
+class Router
+{
+    public function __construct()
+    {
+        echo 'Привет мир!';
+    }
+}
